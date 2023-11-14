99820231279b23d33c6e8eb074cfc83ef30c09a9
SaneManiacMG <mmoroane@hotmail.com>
2023-11-14 09:15:01 +0200

    Add focus to input field in Focus component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Focus from "./components/Focus";
 
--- src/components/Focus.js
+++ src/components/Focus.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Focus = () => {
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+		inputRef.current.focus();
+    }, []);
+    
 	return (
 		<div>
-			<input type="text" />
+			<input ref={inputRef} type="text" />
 		</div>
 	);
 };
